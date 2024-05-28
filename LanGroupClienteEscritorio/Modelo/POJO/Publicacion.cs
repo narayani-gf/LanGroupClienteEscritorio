ff763ff4f9e66a39ba2b95e00f9f5ef11998a266
Froylan-Aarkan <77468347+Froylan-Aarkan@users.noreply.github.com>
2024-05-28 09:38:59 -0600

    Create Publicacion.cs

---
+++ LanGroupClienteEscritorio/Modelo/POJO/Publicacion.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LanGroupClienteEscritorio.Modelo.POJO
+{
+    /* ======================================================================
+    * == Autor(es): Froylan De Jesus Alvarez Rodriguez                     ==
+    * == Fecha de actualización: 28/05/2024                                ==
+    * == Descripción:                                                      ==
+    * =======================================================================
+    */
+    internal class Publicacion
+    {
+        private string idPublicacion { get; set; }
+        private string idColaborador { get; set; }
+        private string idGrupo { get; set; }
+        private string titulo { get; set; }
+        private string descripcion { get; set; }
+        private DateTime fecha { get; set; }
+
+        public Publicacion()
+        {
+        }
+
+        public Publicacion(string idPublicacion, string idColaborador, string idGrupo, string titulo, string descripcion, DateTime fecha)
+        {
+            this.idPublicacion = idPublicacion;
+            this.idColaborador = idColaborador;
+            this.idGrupo = idGrupo;
+            this.titulo = titulo;
+            this.descripcion = descripcion;
+            this.fecha = fecha;
+        }
+    }
+}
