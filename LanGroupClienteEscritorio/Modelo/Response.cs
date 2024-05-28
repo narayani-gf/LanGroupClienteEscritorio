d1174d8af83ecad1dcc2b6368e57fdc83e927134
Narayani <fa.narayani@gmail.com>
2024-05-28 10:34:05 -0600

    agregando POJO y Servicios de Autorización

---
+++ LanGroupClienteEscritorio/Modelo/Response.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LanGroupClienteEscritorio.Modelos
+{
+    public class Response
+    {
+        [JsonProperty("correo")]
+        public string Correo { get; set; }
+
+        [JsonProperty("usuario")]
+        public string Usuario { get; set;}
+
+        [JsonProperty("rol")]
+        public string Rol {  get; set; }
+
+        [JsonProperty("jwt")]
+        public string Jwt { get; set; }
+
+        [JsonProperty("codigo")]
+        public int Codigo { get; set; }
+    }
+}
